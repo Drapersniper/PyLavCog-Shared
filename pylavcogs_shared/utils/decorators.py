721f59173973dc04ca8e2a99931e078c3ce60b03
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-08-06 11:51:26 +0100

    improve DJ

--- pylavcogs_shared/utils/decorators.py
+++ pylavcogs_shared/utils/decorators.py
@@ -8,7 +8,7 @@ from redbot.core.i18n import Translator
 from pylav.utils import PyLavContext
 
 from pylavcogs_shared import errors
-from pylavcogs_shared.errors import UnauthorizedChannelError
+from pylavcogs_shared.errors import NotDJError, UnauthorizedChannelError
 
 _ = Translator("PyLavShared", Path(__file__))
 
@@ -52,19 +52,26 @@ def can_run_command_in_channel():
     return commands.check(pred)
 
 
+async def is_dj_logic(context: PyLavContext) -> bool | type[NotDJError]:
+    if not (getattr(context, "lavalink", None) and context.guild):
+        return False
+    if await context.bot.allowed_by_whitelist_blacklist(who=context.author, guild=context.author.guild):
+        return True
+    is_dj = await context.lavalink.is_dj(
+        user=context.author, guild=context.guild, additional_role_ids=None, additional_user_ids=None
+    )
+    if not is_dj:
+        return errors.NotDJError
+    return True
+
+
 def invoker_is_dj():
     async def pred(context: PyLavContext):
-        if not (getattr(context, "lavalink", None) and context.guild):
-            return False
-        if await context.bot.allowed_by_whitelist_blacklist(who=context.author, guild=context.author.guild):
-            return True
-        is_dj = await context.lavalink.is_dj(
-            user=context.author, guild=context.guild, additional_role_ids=None, additional_user_ids=None
-        )  # type:ignore
-        if not is_dj:
-            raise errors.NotDJError(
-                context,
-            )
-        return True
+        is_dj = await is_dj_logic(context)
+        if isinstance(is_dj, bool):
+            return is_dj
+        raise is_dj(
+            context,
+        )
 
     return commands.check(pred)
